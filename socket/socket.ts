fe024f0c25ee9eefaca5620530739668a94dd660
Judahh <judahholanda7@gmail.com>
2019-07-17 20:20:59 -0300

    journal

--- socket/socket.ts
+++ socket/socket.ts
@@ -1,12 +1,12 @@
 import * as io from 'socket.io-client';
 import { BasicSocket } from './basicSocket';
+import { Journaly } from 'journaly';
 const IV_LENGTH = 16; // For AES, this is always 16
 
 export class Socket {
     private basicSocket: BasicSocket;
     private key;
-    private subscribers: any;
-    private subscribersOldData: any;
+    private journaly: Journaly;
 
     constructor(socket?) {
         let _self = this;
@@ -14,8 +14,7 @@ export class Socket {
             _self.basicSocket = new BasicSocket(socket);
         else
             _self.basicSocket = new BasicSocket(io());
-        _self.subscribers = {};
-        _self.subscribersOldData = {};
+        _self.journaly = new Journaly(true);
         
         _self.basicSocket.on('regularMessage', (data) => {
             _self.publish(data.subscribers, data.data);
@@ -27,19 +26,11 @@ export class Socket {
     }
 
     public subscribe(subscribers, callback) {
-        this.checkSubscribers(subscribers);
-        this.subscribers[subscribers].push(callback);
-        this.subscribersOldData[subscribers].forEach((data) => {
-            callback(data);
-        });
-        console.log(callback.name, 'has been subscribed to', subscribers);
+        this.journaly.subscribe(subscribers,callback);
     }
 
     public unsubscribe(subscribers, callback) {
-        this.checkSubscribers(subscribers);
-        this.subscribers[subscribers] = this.subscribers[subscribers].filter((element) => {
-            return element !== callback;
-        });
+        this.journaly.unsubscribe(subscribers, callback);
     }
 
     public emit(subscribers, data){
@@ -47,17 +38,6 @@ export class Socket {
     }
 
     private publish(subscribers, data) {
-        this.checkSubscribers(subscribers);
-        this.subscribers[subscribers].forEach((subscriber) => {
-            subscriber(data);
-        });
-        this.subscribersOldData[subscribers].push(data);
-    }
-
-    private checkSubscribers(subscribers) {
-        if (this.subscribers[subscribers] === undefined) {
-            this.subscribers[subscribers] = new Array<any>();
-            this.subscribersOldData[subscribers] = new Array<any>();
-        }
+        this.journaly.publish(subscribers, data);
     }
 }
